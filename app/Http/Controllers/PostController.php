c0e900c03deb795f3bf899a51034817b1c21414f
mildredManrique <mildred.manrique@outlook.es>
2022-06-03 16:18:09 -0500

    Se crea el PostController y su ruta en web.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index ()
+    {
+        return view('index', [
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
 }
